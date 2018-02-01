c625b6ea90c2e5220abc89a606a923a4830eaf3a
Kiefer von Gaza <kvongaza@ualberta.ca>
2018-01-31 18:25:40 -0700

    exception class

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetTooLongException.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetTooLongException.java
@@ -1,7 +1,18 @@
 package ca.ualberta.cs.lonelytwitter;
 
-/**
+/**Copyright 2018 Kiefer von Gaza
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+ is furnished to do so, subject to the following conditions:
+
  * Created by sajediba on 1/17/18.
+ */
+
+/**
+ * Represents a class that defines if a tweet is too long
  */
 
 public class TweetTooLongException extends Exception {
